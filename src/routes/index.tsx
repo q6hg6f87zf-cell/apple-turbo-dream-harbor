695cdcdb24c85e21ee95cffb98b72c482e0e07a6
q6hg6f87zf-cell <brent8320@gmail.com>
2026-09-13 08:49:42 -0600

    Bootstrap Tyrone contextual help

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,9 +5,11 @@ import { ClassAliasLayer } from "@/components/game/class-alias-layer";
 import { HollowRealmVisuals } from "@/components/game/hollow-visuals";
 import { ResidentProgressionRuntime } from "@/components/game/resident-progression-runtime";
 import { bootstrapCampaignBalance } from "@/game/balance-bootstrap";
+import { bootstrapTyroneHelp } from "@/game/tyrone-help-bootstrap";
 import { createFileRoute } from "@tanstack/react-router";
 
 bootstrapCampaignBalance();
+bootstrapTyroneHelp();
 
 export const Route = createFileRoute("/")({ component: Home });
 
