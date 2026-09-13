164f319e025746dd738976eb2f23e49a0c8efd40
q6hg6f87zf-cell <brent8320@gmail.com>
2026-09-13 09:24:45 -0600

    Mount authoritative loadout effects runtime

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,7 @@ import { CampaignBalancePanel } from "@/components/game/campaign-balance-panel";
 import { CampaignBalanceRuntime } from "@/components/game/campaign-balance-runtime";
 import { ClassAliasLayer } from "@/components/game/class-alias-layer";
 import { HollowRealmVisuals } from "@/components/game/hollow-visuals";
+import { LoadoutEffectsRuntime } from "@/components/game/loadout-effects-runtime";
 import { ResidentProgressionRuntime } from "@/components/game/resident-progression-runtime";
 import { bootstrapCampaignBalance } from "@/game/balance-bootstrap";
 import { bootstrapTyroneHelp } from "@/game/tyrone-help-bootstrap";
@@ -21,6 +22,7 @@ function Home() {
       <ClassAliasLayer />
       <CampaignBalanceRuntime />
       <ResidentProgressionRuntime />
+      <LoadoutEffectsRuntime />
       <CampaignBalancePanel />
     </>
   );
