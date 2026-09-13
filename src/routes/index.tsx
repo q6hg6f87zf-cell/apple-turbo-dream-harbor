764ad48b4155c785812bcfd7a4e3b34d592f80ea
q6hg6f87zf-cell <brent8320@gmail.com>
2026-09-13 07:15:30 -0600

    Wire Hollow Realm artwork into live app

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,14 @@
-import { createFileRoute } from "@tanstack/react-router";
 import { GameApp } from "@/components/game/App";
+import { HollowRealmVisuals } from "@/components/game/hollow-visuals";
+import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({ component: Home });
 
 function Home() {
-  return <GameApp />;
+  return (
+    <>
+      <GameApp />
+      <HollowRealmVisuals />
+    </>
+  );
 }
